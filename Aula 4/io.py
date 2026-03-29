af2b3065eb9c112d42cd50ae0346f0acb2944384
Fabioomega <fabioms2002@gmail.com>
2026-03-29 18:19:50 -0300

    Added aula 4 content

---
+++ Aula 4/io.py
@@ -0,0 +1,9 @@
+# Lendo arquivos
+
+# Salvando arquivos
+
+# O que é um JSON (é um dicionario)
+
+# Lendo um JSON
+
+# Carregando um JSON
---
+++ Aula 4/numpy.py
@@ -0,0 +1,15 @@
+# Criando uma array com arrange
+
+# Vendo propiedades de uma array (shape, len, flatten)
+
+# Mudando o formato de uma array (reshape)
+
+# Indexando uma array multidimensional
+
+# Slicing the uma array multidimensional
+
+# O que é uma mascara
+
+# Mascaras
+
+# Mudando dados via mascaras
